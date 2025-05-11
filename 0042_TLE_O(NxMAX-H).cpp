7be282538437e0efd130b0eb3b113b503d035648
shazilkp <155661577+shazilkp@users.noreply.github.com>
2025-05-11 23:29:59 +0530

    Update 0042_TLE_O(NxMAX-H).cpp

--- 0042_TLE_O(NxMAX-H).cpp
+++ 0042_TLE_O(NxMAX-H).cpp
@@ -4,11 +4,13 @@ public:
         int n = height.size();
         int totWat = 0;
         int start = 0;
-        int end = 0;
+        int end = n;
         while(1){       //layer loop
             int sum = 0;
             int f=0;
-            for(int i = 0 ; i < n ; i ++){
+            int oldEnd = end;
+            int oldStart = start;
+            for(int i = oldStart ; i <= oldEnd ; i ++){
                 if(height[i] > 0){
                     if(f == 0){
                         f=1;
@@ -18,6 +20,7 @@ public:
                 }
                 sum+=height[i];
             }
+            //cout << start << " " << end << "\n";
 
             if(sum == 0 || sum == 1){
                 break;
